b04bf31829fed494210ce0eb53c928f486c24d47
Pentonny <https://github.com/daehoon0917/Oneday_Diary.git>
2022-07-01 13:52:01 +0900

    2일차

--- app/src/main/java/com/example/calmemo/MainActivity.java
+++ app/src/main/java/com/example/calmemo/MainActivity.java
@@ -14,56 +14,47 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private AppCompatActivity binding;
     public CalendarView calendarView;
-    public CalendarView dayOfMonth;
-    public View.OnClickListener onLongClick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CalendarView calendar = (CalendarView) findViewById(R.id.CalendarView);
+        calendarView = findViewById(R.id.CalendarView);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.item1, R.id.item2, R.id.item3, R.id.item4).build();
-        final CharSequence[] date = {null};
 
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        calendarView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                    PopupMenu popup = new PopupMenu(MainActivity.this, view);
-                    getMenuInflater().inflate(R.menu.option_menu, popup.getMenu());
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(getApplicationContext(), view);
+                getMenuInflater().inflate(R.menu.option_menu, popup.getMenu());
 
-                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener(){
-                        public boolean onMenuItemClick(MenuItem item){
-                            switch (item.getItemId()){
-                                case R.id.Oitem1:
-                                    Intent diaryintent = new Intent(getApplicationContext(), DiaryActivity.class);
-                                    startActivity(diaryintent);
-                                    break;
-                                case R.id.Oitem2:
-                                    Intent memointent = new Intent(getApplicationContext(), MemoActivity.class);
-                                    startActivity(memointent);
-                                    break;
-                                case R.id.Oitem3:
-                                    Intent calendarintent = new Intent(getApplicationContext(), CalendarActivity.class);
-                                    startActivity(calendarintent);
-                                    break;
-                            }
-                            return false;
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.Oitem1:
+                                Intent diaryintent = new Intent(getApplicationContext(), DiaryActivity.class);
+                                startActivity(diaryintent);
+                                break;
+                            case R.id.Oitem2:
+                                Intent memointent = new Intent(getApplicationContext(), MemoActivity.class);
+                                startActivity(memointent);
+                                break;
+                            case R.id.Oitem3:
+                                Intent calendarintent = new Intent(getApplicationContext(), CalendarActivity.class);
+                                startActivity(calendarintent);
+                                break;
                         }
-                    });
-                    popup.show();
+                        return false;
+                    }
+                });
             }
         });
-
     }
-
-
-
-
 }
